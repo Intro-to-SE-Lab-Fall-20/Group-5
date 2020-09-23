f6eef01cdc5f17e09a55d4f896f503f3ed514dd5
haleyhagler227 <70032753+haleyhagler227@users.noreply.github.com>
2020-09-23 13:27:51 -0500

    Rename login_credentials.py to app/login_credentials.py
    Moved login_credentials.py into folder app to fix error

---
+++ app/login_credentials.py
@@ -0,0 +1,5 @@
+
+
+user_name = "" # To make this work add email you wish to send emails from
+
+password=""	# Add password for the email account, I recomment only running this on local machine not deploying to Heroku

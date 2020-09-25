9a296cb82ceab8fab7c519f59815ab04f618458c
haleyhagler227 <70032753+haleyhagler227@users.noreply.github.com>
2020-09-25 17:02:36 -0500

    Update smtp.py

--- app/smtp.py
+++ app/smtp.py
@@ -1,19 +1,7 @@
 import smtplib, ssl
 from app.models import Email
 
-'''port = 465  # For SSL
-smtp_server = "smtp.gmail.com"
-sender_email = "HurleyIsTheBestDog@gmail.com"  # Enter your address
-receiver_email = "haleyhagler@gmail.com"  # Enter receiver address
-password = 'Hurley23'
-message = """From: From Person <from@fromdomain.com>
-To: To Person <haleyhagler@gmail.com>
-Subject: SMTP e-mail test
 
-This is a test e-mail message.
-
-"""
-'''
 
 def sendemail(smtp_server, port, email):
 

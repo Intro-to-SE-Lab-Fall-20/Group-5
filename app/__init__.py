52e5820992fe7f42592294e54fdc86e793b8a16a
haleyhagler227 <70032753+haleyhagler227@users.noreply.github.com>
2020-09-25 19:39:19 -0500

    Editing the include new changes to the mast

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
-
+from config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config.from_object(Config)
 
-from app import routes #it is import this line goes after app =
-
+from app import routes
 
 from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap(app)
--- app/models.py
+++ app/models.py
@@ -3,13 +3,12 @@
 
 class Email:
 	
-	def __init__(self, sender, reciever, subject, message, i,  password): 
+	def __init__(self, sender, reciever, subject, message, password): 
 		self.sender = sender
 		self.reciever = reciever
 		self.subject = subject
 		self.message = message
 		self.password = password
-		self.id = i
 	#html = html  Commented out until able to recieve emails
 
 	def __repr__(self):
@@ -17,11 +16,3 @@ class Email:
 
 	
 
-	def serialize(self):
-		return { 
-			"sender" : self.sender,
-			'reciever': self.reciever,
-			'subject': self.subject,
-			'body': self.message,
-			'id': self.id
-		}

7a6e5f75b9dc2b0ee6b45301da18ac5cc747d3a5
haleyhagler227 <70032753+haleyhagler227@users.noreply.github.com>
2020-10-23 11:32:23 -0500

    Forward email button, multi-threading

--- app/smtp.py
+++ app/smtp.py
@@ -1,14 +1,44 @@
 import smtplib, ssl
 from app.models import Email
 
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import html2text
 
+
+from threading import Thread
+
+class EmailThread(Thread):
+	def __init__(self, smtp_server, port, email):
+		self.email_to = email.reciever
+		self.email_from = email.sender
+		self.email = email.message
+		self.subject = email.subject
+		self.port = port
+		self.smtp_server = smtp_server
+		self.password = email.password
+		Thread.__init__(self)
+
+	def run (self):
+
+		message = MIMEMultipart("alternative")
+		
+		message["Subject"] = self.subject
+		message["From"] = self.email_from
+		message["To"] = self.email_to
+
+		part1 = MIMEText(html2text.html2text(self.email), "plain")
+		part2 = MIMEText(self.email, "html")
+
+		print("Created message")
+		
+		context = ssl.create_default_context()
+		with smtplib.SMTP_SSL(self.smtp_server, self.port, context=context) as server:
+			server.login(self.email_from, self.password)
+			server.sendmail(self.email_from, self.email_to, message)
+
+		print("SEnt?")
 
 def sendemail(smtp_server, port, email):
 
-
-	message = "From: <" + email.sender + "> \nTo: <" + email.reciever + "> \nSubject: " + email.subject + " \n" + email.message
-
-	context = ssl.create_default_context()
-	with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-		server.login(email.sender, email.password)
-		server.sendmail(email.sender, email.reciever, message)
+	EmailThread(smtp_server, port, email).start()

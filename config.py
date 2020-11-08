2a41a9590349cdad6bd732e190f8994f32452521
haleyhagler227 <70032753+haleyhagler227@users.noreply.github.com>
2020-11-07 18:35:29 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
 
 class Config(object):
-	TESTING = True
-	DEBUG = True
-	FLASK_ENV = 'development'
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'guess-my-key'
-	PYTHONPATH= "${PYTHONPATH}:" + os.getcwd()
-
+	
